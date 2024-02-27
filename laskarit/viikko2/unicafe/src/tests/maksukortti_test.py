276d950a619d9faee271f940da673953a3a4478e
Aineopintojen-harjoitustyo-ohjelmistote <github-hy-ohte@v.hix.fi>
2024-02-27 13:21:22 +0200

    Viikko 2: Tehtävä 6: Takaisin testeihin

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,24 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_on_oikein_aluksi(self):
+        self.assertEqual(self.maksukortti.saldo, 1000)
+
+    def test_kortin_lataaminen_kasvattaa_saldoa(self):
+        self.maksukortti.lataa_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo, 1500)
+
+    def test_rahan_ottaminen_vähentää_saldoa(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo, 500)
+        
+    def test_rahan_ottaminen_ei_onnistu_ilman_saldoa(self):
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(self.maksukortti.saldo, 1000)
+        
+    def test_rahan_ottaminen_palauttaa_oikein(self):
+        self.assertTrue(self.maksukortti.ota_rahaa(500))
+        self.assertTrue(self.maksukortti.ota_rahaa(500))
+        self.assertFalse(self.maksukortti.ota_rahaa(500))
+        
